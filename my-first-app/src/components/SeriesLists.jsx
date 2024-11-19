7e358e1627f8bfa135a649fd0d9de7960ba452ff
najafhanif04 <najafhanif29@gmail.com>
2024-11-20 00:27:33 +0500

    react router dom

--- my-first-app/src/components/SeriesLists.jsx
+++ my-first-app/src/components/SeriesLists.jsx
@@ -11,12 +11,12 @@ export default function SeriesLists({data}) {
         <img src={img_url} width={500} />
       </div>
       <h2>Name: {name}</h2>
-      <h3>Rating: {rating}</h3>
+      <h3> Rating: <span className={rating >= 8.5 ? 'text-emerald-400' : 'text-red-400'}>{rating}</span> </h3>
       <p className="text-sm">Description: {description}</p>
       <p>Genre: {genre}</p>
       <p>Cast: {cast}</p>
       <a
-        className="bg-red-800 p-2 inline-block hover:bg-slate-950"
+        className={rating >=8.5 ? 'bg-emerald-800 p-2 inline-block hover:bg-slate-950' : 'bg-red-800 p-2 inline-block hover:bg-slate-950'}
         href={watch_url}
         target="_blank"
       >
